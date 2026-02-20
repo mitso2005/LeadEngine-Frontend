e68760fbb67a94a9dde90b7e481d157dde7c4423
Dimitri Petrakis <dimitripetrakisvic@gmail.com>
2026-02-20 13:29:57 +1100

    clean up

--- src/components/field.tsx
+++ src/components/field.tsx
@@ -2,9 +2,10 @@ import React, { useState } from 'react';
 
 type FieldProps = {
     placeholder?: string; // Optional placeholder prop
+    variable?: string; // Optional variable prop
 };
 
-const Field: React.FC<FieldProps> = ({ placeholder }) => {
+const Field: React.FC<FieldProps> = ({ placeholder, variable }) => {
 
     const [value, setValue] = useState<string>('');
 
@@ -16,9 +17,8 @@ const Field: React.FC<FieldProps> = ({ placeholder }) => {
     return (
 
         <div>
-            <label htmlFor="field">Field:</label>
+            <label htmlFor="field">{variable}:  </label>
             <input type="text" id="field" value={value} onChange={handleChange} placeholder={placeholder}/>
-            <p>Current value: {value}</p>
         </div>
     );
 };
